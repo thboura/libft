527563829bf087180155284344949278c257d2d0
thboura <thomas.boura@gmail.com>
2020-08-13 16:45:46 +0200

    Documentation of gnl & conv functions

--- hdrs/libft_conv.h
+++ hdrs/libft_conv.h
@@ -6,7 +6,7 @@
 /*   By: thboura <thboura@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/18 11:56:35 by kguibout          #+#    #+#             */
-/*   Updated: 2020/08/10 19:09:16 by thboura          ###   ########.fr       */
+/*   Updated: 2020/08/13 16:44:20 by thboura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,15 @@
 
 # include "libft.h"
 
+/**
+ * \typedef	t_atof
+ * \brief	Typedef of s_atof.
+ */
 typedef struct s_atof	t_atof;
+/**
+ * \struct	s_atof
+ * \brief	ft_atof() handler.
+ */
 struct					s_atof
 {
 	bool	sign;
@@ -34,12 +42,47 @@ struct					s_atof
 	int		frac2;
 };
 
+/**
+ * \fn		long ft_atol(const char *str)
+ * \brief	Convert a string to a long.
+ * \return	Long value.
+ */
 long					ft_atol(const char *str);
+/**
+ * \fn		float ft_atof(const char *str)
+ * \brief	Convert a string to a float.
+ * \return	Float value.
+ */
 float					ft_atof(const char *str);
+/**
+ * \fn		int ft_atoi(const char *nptr)
+ * \brief	Reproduction of atoi() function (convert a string to an int).
+ * \return	Int value.
+ */
 int						ft_atoi(const char *nptr);
+/**
+ * \fn		t_u32 ft_atoi_hex(char *str)
+ * \brief	Convert a string to an unsigned int as hexadecimal value.
+ * \return	Unsigned int value.
+ */
 t_u32					ft_atoi_hex(char *str);
+/**
+ * \fn		char *ft_itoa(int n)
+ * \brief	Convert an int to a string.
+ * \return	Pointer on string.
+ */
 char					*ft_itoa(int n);
+/**
+ * \fn		char *ft_lftoa(int n)
+ * \brief	Convert a double to a string.
+ * \return	Pointer on string.
+ */
 char					*ft_lftoa(double lf);
+/**
+ * \fn		char *ft_itoa(int n)
+ * \brief	Convert an unsigned int to a string (with given base).
+ * \return	Pointer on string.
+ */
 char					*ft_uitoa_base(t_u64 n, const char *base);
 
 #endif
--- hdrs/libft_gnl.h
+++ hdrs/libft_gnl.h
@@ -3,18 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   libft_gnl.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kguibout <kguibout@student.42.fr>          +#+  +:+       +#+        */
+/*   By: thboura <thboura@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 15:42:00 by kguibout          #+#    #+#             */
-/*   Updated: 2019/01/07 15:53:00 by kguibout         ###   ########.fr       */
+/*   Updated: 2020/08/13 16:18:57 by thboura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_GNL_H
 # define LIBFT_GNL_H
 
+/**
+ * \def		BUFF_SIZE
+ * \brief	Size of get_next_line() buffer.
+ */
 # define BUFF_SIZE 4096
 
+/**
+ * \fn		int get_next_line(const int fd, char **line)
+ * \brief	Returns a line read from a file descriptor.
+ * \param	fd File descriptor that will be used to read
+ * \param	line Address of a string that will be used to save the line read
+ * 		from the file descriptor
+ * \return	1 if a line has been read, 0 if the reading has been completed,
+ * 		or -1 if an error has happened respectively.
+ * \note	This function returns its result without '\\n'.
+ */
 int	get_next_line(const int fd, char **line);
 
 #endif
